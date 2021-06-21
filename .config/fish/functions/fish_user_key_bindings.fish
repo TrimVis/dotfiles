f8381ee4a1057e4e662e97e74a9fe4958aae6be6
Pasquale Jordan <jordanpa@ethz.ch>
2021-06-21 22:44:17 +0200

    Added things like !! and qt engine

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,27 @@
 function fish_user_key_bindings
     fish_vi_key_bindings
     bind -M insert \cf accept-autosuggestion
+    bind -M insert ! bind_bang
+    bind -M insert '$' bind_dollar
+end
+
+
+function bind_bang
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]
+    commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function bind_dollar
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
 end
