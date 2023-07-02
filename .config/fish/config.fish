5c4b056c6f409983462093166c5e12aaa467359c
Pasquale Jordan <jordanpa@ethz.ch>
2023-07-02 21:00:21 +0200

    Various other changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -29,3 +29,4 @@ if set -q KITTY_INSTALLATION_DIR
     source "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish"
     set --prepend fish_complete_path "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_completions.d"
 end
+
--- .config/fish/functions/xssh.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function xssh --wraps='ssh' --description 'alias xssh = TERM=xterm-256color ssh'
-  TERM=xterm-256color ssh $argv;
-end
